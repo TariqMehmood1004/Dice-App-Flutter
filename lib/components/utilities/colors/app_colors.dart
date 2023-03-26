ab4b6370128cb4c5ea61c92c539f267f5a533976
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2023-03-27 00:36:37 +0500

    2nd commit

---
+++ lib/components/utilities/colors/app_colors.dart
@@ -0,0 +1,41 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+class AppColorController {
+  static Color backgroundLight = const Color(0xffffffff);
+  static Color white = const Color.fromARGB(255, 255, 255, 255);
+
+  static Color blueLight = const Color(0xff733aef);
+  static Color chocolateColor = const Color(0xff1c0613);
+  static Color highChocolateColor = const Color(0xff0a0c0b);
+  static Color greenColor = const Color(0xff3c495a);
+  static Color greyColor = const Color(0xff261725);
+  static Color reddish = const Color(0xff140309);
+  static Color yellowOff = const Color(0xffe89457);
+  static Color offWhite = const Color(0xffa8acab);
+  static Color goldGrain = const Color(0xffb97e43);
+  static Color orange = const Color(0xffefb13e);
+  static Color darkBlue = const Color(0xff0ebcee);
+  static Color prettyBlue = const Color(0xff002c53);
+  static Color prettyOrange = const Color(0xffa9671f);
+
+  static Color backgroundLightGrey = const Color(0xfff2f3f4);
+  static Color black = const Color(0xff070508);
+  static Color backgroundDark = const Color(0xFF000000);
+  static Color backgroundDarkTransparented = const Color(0xB8000000);
+  static Color iconTint = const Color(0xff888787);
+  static Color navigationSelectionColor = const Color(0xffffc107);
+  static Color cardBGColor = const Color(0xfffbfbfb);
+  static Color transparent = const Color(0x00FBFBFB);
+  static Color blueBlack = const Color(0xFF060D1D);
+  static Color blueBlackOff = const Color(0xDF09142D);
+  static Color blackTransparent = const Color.fromARGB(206, 0, 0, 0);
+  static Color pink = const Color(0xFFDBD4FA);
+
+  static Color colorBox1 = const Color(0xFF2A2738);
+
+  static Color palleteColor = const Color(0xFF4a021a);
+  static Color palleteColor1 = const Color(0xFF260615);
+
+  static Color colorBox2 = const Color(0xFFf3f4f9);
+}
